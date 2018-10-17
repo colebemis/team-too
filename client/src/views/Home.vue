bfca080fd2019ad5432a100d26cbd9821385eb74
Sujit <smadiraj@calpoly.edu>
2018-10-16 19:40:35 -0700

    Update Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="FOXYCLE 2"/>
+
+    <!-- Apollo watched Graphql query -->
+    <ApolloQuery
+      :query="require('../graphql/User.gql')"
+    >
+      <template slot-scope="{ result: { loading, error, data } }">
+
+        <!-- Result -->
+        <div v-for="user of data.users" :key="user.name" class="result apollo">{{ user.name}}</div>
+
+      </template>
+    </ApolloQuery>
+
+    <el-button>Default</el-button>
   </div>
+
+
 </template>
 
 <script lang="ts">
