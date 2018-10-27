879131f62fde00196c038c23d87b37e46daee45d
Sujit <smadiraj@calpoly.edu>
2018-10-27 14:40:29 -0700

    Added test for product query

---
+++ server/src/__tests__/productQuery.test.ts
@@ -0,0 +1,28 @@
+import { gql } from "apollo-server";
+import { graphql } from "graphql";
+import { importSchema } from "graphql-import";
+import { addMockFunctionsToSchema, makeExecutableSchema } from "graphql-tools";
+import * as path from "path";
+
+// Set up and mock graphql server
+const typeDefs = gql(
+  importSchema(path.resolve(__dirname, "../schema.graphql")),
+);
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+
+it("returns user name and email", async () => {
+  const query = `
+    query  {
+      products {
+        name,
+        id,
+        price
+      }
+    }
+  `;
+
+  const result = await graphql(schema, query);
+
+  expect(result).toMatchSnapshot();
+});
