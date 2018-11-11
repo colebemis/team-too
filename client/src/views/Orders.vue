84b6e72f9843b076034125a6bbe12dd3057bacb4
Gabriel Medina-Kim <gmedinak@calpoly.edu>
2018-11-11 12:33:49 -0800

    orders added to table

--- client/src/views/Orders.vue
+++ client/src/views/Orders.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
-    <h1>Orders</h1>
-    <div v-for="order in orders" :key="order.id">
-      <router-link :to="{ path: `/admin/order/${order.id}` }">
-        {{ order.id }}
-      </router-link>
-    </div>
+    <PageHeader>Orders</PageHeader>
+
+    <table>
+      <tr>
+        <th>Order</th>
+        <th>Status</th>
+        <th>Type</th>
+        <th>Date</th>
+        <th>Customer</th>
+      </tr>
+
+      <tr v-for="order in orders" :key="order.id">
+        <td>
+          <router-link :to="{ path: `/admin/order/${order.id}` }">
+            {{ order.id }}
+          </router-link>
+        </td>
+        <td>{{ order.status }}</td>
+        <td>{{ order.status }}</td>
+        <td>{{ order.createdAt }}</td>
+        <td>{{ order.customer.name }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 import gql from "graphql-tag";
+import PageHeader from "@/components/PageHeader.vue";
 
 export default {
+  components: { PageHeader },
   data() {
     return {
       orders: [],
@@ -22,10 +41,30 @@ export default {
     orders: gql`
       query orders {
         orders {
-          id
+          id,
+          status,
+          createdAt,
+          customer{name}
         }
       }
     `,
   },
 };
 </script>
+
+<style scoped>
+.gridHeader {
+  display: grid;
+  padding: 10px;
+  grid-template-columns: repeat(10, 1fr);
+  font-weight: bold;
+}
+
+.gridHeader > div:nth-child(2) {
+  grid-column: 7;
+}
+
+.gridHeader > div:nth-child(3) {
+  grid-column: 10;
+}
+</style>
