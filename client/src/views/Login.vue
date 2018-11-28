a68841cbda93ba35f3afd025e3c924c040b6ced4
Cole Bemis <colebemis@gmail.com>
2018-11-27 22:12:00 -0800

    Display message if user is logged in

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,45 +3,54 @@
     <PageHeader>Login</PageHeader>
     <div class="max-w-xs mx-auto mt-16 mb-24">
       <div
-        v-if="error"
-        data-test-id="error"
-        class="p-4 text-red-darker leading-normal bg-red-lightest mb-8"
+        v-if="isLoggedIn"
+        data-test-id="logged-in"
+        class="text-black text-center"
       >
-        {{ error.message }}
+        You are already logged in.
       </div>
-      <form id="login-form" @submit="logIn">
-        <div class="mb-8">
-          <label for="email" class="inline-block font-semibold mb-2">
-            Email
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            v-model="email"
-            placeholder="bill@example.com"
-            class="w-full p-3 border border-grey"
-            required
-          />
+      <div v-else>
+        <div
+          v-if="error"
+          data-test-id="error"
+          class="p-4 text-red-darker leading-normal bg-red-lightest mb-8"
+        >
+          {{ error.message }}
         </div>
-        <div>
-          <label for="password" class="inline-block font-semibold mb-2">
-            Password
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            v-model="password"
-            placeholder="•••••"
-            class="w-full p-3 border border-grey"
-            required
-          />
-        </div>
-        <div class="flex flex-col items-stretch mt-12">
-          <Button data-test-id="login-button">Log in</Button>
-        </div>
-      </form>
+        <form id="login-form" @submit="logIn">
+          <div class="mb-8">
+            <label for="email" class="inline-block font-semibold mb-2">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              v-model="email"
+              placeholder="bill@example.com"
+              class="w-full p-3 border border-grey"
+              required
+            />
+          </div>
+          <div>
+            <label for="password" class="inline-block font-semibold mb-2">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              v-model="password"
+              placeholder="•••••"
+              class="w-full p-3 border border-grey"
+              required
+            />
+          </div>
+          <div class="flex flex-col items-stretch mt-12">
+            <Button data-test-id="login-button">Log in</Button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -51,7 +60,7 @@ import Vue from "vue";
 import gql from "graphql-tag";
 import PageHeader from "@/components/PageHeader.vue";
 import Button from "@/components/Button.vue";
-import { onLogin } from "../vue-apollo";
+import { logIn, isLoggedIn } from "@/auth.ts";
 
 export default Vue.extend({
   components: { PageHeader, Button },
@@ -60,7 +69,11 @@ export default Vue.extend({
       error: null,
       email: "",
       password: "",
+      isLoggedIn: false,
     };
+  },
+  mounted() {
+    this.isLoggedIn = isLoggedIn();
   },
   methods: {
     logIn(event) {
@@ -84,13 +97,16 @@ export default Vue.extend({
             password: this.password,
           },
         })
-        .then(data => {
-          // Save user data in localStorage
-          localStorage.setItem("user", JSON.stringify(data.data.logIn.user));
-          // Apollo will save the token and use it to authorize all future requests
-          onLogin(this.$apollo.provider.defaultClient, data.data.logIn.token);
+        .then(({ data }) => {
+          logIn(
+            data.logIn.token,
+            data.logIn.user,
+            this.$apollo.provider.defaultClient,
+          );
           // Redirect to homepage
-          this.$router.push("/");
+          window.location = "/";
+          // We use window.location instead of $router.push to trigger a page refresh
+          // so any components that use localStorage get updated
         })
         .catch(error => (this.error = error));
     },
