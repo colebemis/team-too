31d1d655042b2fd45c80fabeb1888ebc2fd82fd4
Cole Bemis <colebemis@gmail.com>
2018-11-28 22:44:12 -0800

    Add create user form

--- client/src/views/Accounts.vue
+++ client/src/views/Accounts.vue
@@ -22,13 +22,13 @@
           <!-- Only edit/delete other users -->
           <div v-if="currentUser.id !== user.id">
             <input
-              @change="updateIsAdmin(user.id, $event.target.checked)"
+              @change="updateIsAdmin(user.id, $event.target.checked);"
               type="checkbox"
-              id="is-admin"
-              name="is-admin"
+              :id="`is-admin-${user.id}`"
+              :name="`is-admin-${user.id}`"
               v-model="user.isAdmin"
-            >
-            <label for="is-admin" class="ml-2 mr-6">Admin</label>
+            />
+            <label :for="`is-admin-${user.id}`" class="ml-2 mr-6">Admin</label>
             <button
               @click="deleteUser(user.id);"
               class="px-3 py-2 text-sm bg-grey-light hover:bg-red hover:text-white"
@@ -39,6 +39,64 @@
           <div v-else class="text-grey-dark">(You)</div>
         </div>
       </div>
+      <div class="mt-6 bg-grey-lightest p-6">
+        <form @submit.prevent="createUser">
+          <div class="mb-8">
+            <label for="name" class="inline-block font-semibold mb-2">
+              Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              v-model="newUser.name"
+              placeholder="Bill Nye"
+              class="w-full p-3 border border-grey"
+              required
+            />
+          </div>
+          <div class="mb-8">
+            <label for="email" class="inline-block font-semibold mb-2">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              v-model="newUser.email"
+              placeholder="bill@example.com"
+              class="w-full p-3 border border-grey"
+              required
+            />
+          </div>
+          <div class="mb-8">
+            <label for="password" class="inline-block font-semibold mb-2">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              v-model="newUser.password"
+              placeholder="•••••"
+              class="w-full p-3 border border-grey"
+              required
+            />
+          </div>
+          <div class="mb-8">
+            <input
+              type="checkbox"
+              id="is-admin"
+              name="is-admin"
+              v-model="newUser.isAdmin"
+            />
+            <label for="is-admin" class="ml-2 mr-6">Admin</label>
+          </div>
+          <div class="flex flex-col items-stretch">
+            <Button data-test-id="create-account">Create account</Button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -48,6 +106,7 @@ import gql from "graphql-tag";
 import Vue from "vue";
 import PageHeader from "@/components/PageHeader.vue";
 import Loader from "@/components/Loader.vue";
+import Button from "@/components/Button.vue";
 
 const USERS = gql`
   query users {
@@ -61,17 +120,23 @@ const USERS = gql`
 `;
 
 export default Vue.extend({
-  components: { PageHeader, Loader },
+  components: { PageHeader, Loader, Button },
   data() {
     return {
       users: [],
-      currentUser: null
+      currentUser: null,
+      newUser: {
+        name: "",
+        email: "",
+        password: "",
+        isAdmin: false,
+      },
     };
   },
   mounted() {
     // This will be replaced with a call to getUser() when my other pull is merged
     if (localStorage.getItem("user")) {
-      this.currentUser = JSON.parse(localStorage.getItem("user"))
+      this.currentUser = JSON.parse(localStorage.getItem("user"));
     }
   },
   apollo: {
@@ -81,7 +146,7 @@ export default Vue.extend({
     deleteUser(userId: string) {
       this.$apollo.mutate({
         mutation: gql`
-          mutation($userId: ID!) {
+          mutation ($userId: ID!) {
             deleteUser(where: { id: $userId }) {
               id
             }
@@ -101,15 +166,40 @@ export default Vue.extend({
     },
     updateIsAdmin(userId: string, isAdmin: boolean) {
       this.$apollo.mutate({
-       mutation: gql`
-         mutation($userId: ID!, $isAdmin: Boolean!) {
-           updateUser(data: { isAdmin: $isAdmin }, where: { id: $userId }) {
-             id
-             isAdmin
-           }
-         }
-       `,
-       variables: { userId, isAdmin },
+        mutation: gql`
+          mutation ($userId: ID!, $isAdmin: Boolean!) {
+            updateUser(data: { isAdmin: $isAdmin }, where: { id: $userId }) {
+              id
+              isAdmin
+            }
+          }
+        `,
+        variables: { userId, isAdmin },
+      });
+    },
+    createUser() {
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation ($data: UserCreateInput!) {
+            createUser(data: $data) {
+              user {
+                id
+                name
+                email
+                isAdmin
+              }
+            }
+          }
+        `,
+        variables: { data: this.newUser },
+        update: (store, { data: { createUser } }) => {
+          // Read the data from our cache for this query.
+          const data = store.readQuery({ query: USERS });
+          // Adds the user to the cache.
+          data.users.push(createUser.user);
+          // Writes the updated query to the cache.
+          store.writeQuery({ query: USERS, data });
+        },
       });
     }
   },
