4f786830ddfc2d49c2fc39c39024855471e8cb1b
Cole Bemis <colebemis@gmail.com>
2018-11-28 21:23:50 -0800

    Add ability to delete user

--- client/src/views/Accounts.vue
+++ client/src/views/Accounts.vue
@@ -8,7 +8,10 @@
         <div
           v-for="(user, index) in users"
           :key="user.id"
-          :class="[index !== users.length - 1 && 'border-b', 'py-6']"
+          :class="[
+            index !== users.length - 1 && 'border-b',
+            'py-6 flex justify-between items-center',
+          ]"
         >
           <div class="flex flex-col">
             <span class="leading-normal">{{ user.name }}</span>
@@ -16,6 +19,12 @@
               user.email
             }}</span>
           </div>
+          <button
+            @click="deleteUser(user.id);"
+            class="px-3 py-2 text-sm bg-grey-light hover:bg-red hover:text-white"
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
@@ -28,6 +37,16 @@ import Vue from "vue";
 import PageHeader from "@/components/PageHeader.vue";
 import Loader from "@/components/Loader.vue";
 
+const USERS = gql`
+  query users {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
+
 export default Vue.extend({
   components: { PageHeader, Loader },
   data() {
@@ -36,15 +55,30 @@ export default Vue.extend({
     };
   },
   apollo: {
-    users: gql`
-      query users {
-        users {
-          id
-          name
-          email
-        }
-      }
-    `,
+    users: USERS,
+  },
+  methods: {
+    deleteUser(userId: string) {
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation($userId: ID!) {
+            deleteUser(where: { id: $userId }) {
+              id
+            }
+          }
+        `,
+        variables: { userId },
+        update: (store, { data: { deleteUser } }) => {
+          // Read the data from our cache for this query.
+          const data = store.readQuery({ query: USERS });
+          // Deletes the user from the cache.
+          const index = data.users.findIndex(user => user.id === deleteUser.id);
+          data.users.splice(index, 1);
+          // Writes the updated query to the cache.
+          store.writeQuery({ query: USERS, data });
+        },
+      });
+    },
   },
 });
 </script>
