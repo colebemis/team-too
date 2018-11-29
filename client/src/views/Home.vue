f1608021cb26268e324e0f92b684a5e21f6b8d72
Derek Lam <dereklam67@gmail.com>
2018-11-28 17:34:12 -0800

    merged in homepage pr

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,71 @@
 <template>
-  <div>
-    <img
-      class="w-full"
-      alt="Main photo"
-      src="https://images.wallpaperscraft.com/image/peugeot_bike_sport_113690_3840x2160.jpg"
-    />
-    <h1 class="text-center">FIND YOUR RIDE.</h1>
+  <div class="text-white">
+    <div class="flex relative overflow-hidden flex-none py-1">
+      <img
+        class="w-full h-full"
+        alt="Main photo"
+        src="http://www.acyclists.com/wp-content/uploads/2017/10/ONeill-0022-6M3A9750-Edit-DUP1.jpg"
+      />
+      <h1
+        class="py-32 mx-16 text-right lg:text-7xl md:text-5xl text-3xl font-normal uppercase absolute pin-x pin-t"
+      >
+        Find your ride.
+      </h1>
+    </div>
+
+    <div class="flex h-80 mb-1  overflow-hidden">
+       <div
+        class="flex w-1/3 flex-none items-center justify-center bg-cover  mr-1"
+        style="background-image: url(http://wallpaperhd.online/upload/images/1527038205118/nature-forest-wallpaper-hd-night-forest-images-night-forest-wallpaper-light-cloud-1280x800.jpg)"
+      >
+        <h1
+          class="lg:text-6xl sm:text-3xl text-lg font-normal text-center uppercase"
+        >
+          About
+        </h1>
+      </div>
+      <div
+        class="flex w-2/3 flex-none items-center justify-center lg:px-24 px-16 bg-cover"
+        style="background-image: url(https://wallpaperaccess.com/full/420374.jpg)"
+      >
+        <p
+          class=" lg:text-lg text-sm text-center leading-normal font-semibold"
+        >
+         Foxycle was created on September 20, 2018, when Kris Fox was teaching
+          307. He decided to make an assignment to have his students develop a
+          website for his pretend store and have them compete for pizza. Maybe
+          one day the website will be used in the real world.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex h-80 mb-1 overflow-hidden">
+      <div
+        class="flex w-2/3 flex-none items-center justify-center mr-1 lg:px-24 px-16 bg-cover"
+        style="background-image: url(https://images.unsplash.com/photo-1496147539180-13929f8aa03a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2428df044d72f0be9086d83b8c90f0b6&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb)"
+      >
+        <p
+          class=" lg:text-lg text-sm text-center leading-normal font-semibold"
+        >
+          We offer various services for your convenience:<br />
+          <ul class="list-reset">
+            <li>Check tire pressure</li>
+            <li>Check tire wear and tear</li>
+            <li>Clean the frame</li>
+            <li>Check bearing system</li>
+          </ul>
+        </p>
+      </div>
+      <div
+        class="flex w-1/3 flex-none items-center justify-center bg-cover"
+        style="background-image: url(https://images.triumphmotorcycles.co.uk/media-library/images/bikes/mountain%20bike/1920x1080-mountain-bike-background.jpg)"
+      >
+        <h1
+          class="lg:text-6xl sm:text-3xl text-lg font-normal text-center uppercase"
+        >
+          Services
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
