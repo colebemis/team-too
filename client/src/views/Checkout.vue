b074cd9ac8e8df86f0186668f919dcfc4698c7f5
Cole Bemis <colebemis@gmail.com>
2018-12-07 18:38:11 -0800

    Fix up order statuses

--- client/src/views/Checkout.vue
+++ client/src/views/Checkout.vue
@@ -482,7 +482,7 @@
                 </div>
               </div>
             </form>
-          </div> 
+          </div>
 
             <!-- CART SUMMARY -->
           <div class="w-1/2 ml-10">
@@ -594,7 +594,7 @@
         </div>
       </div>
     </div>
-  
+
 </template>
 
 <style>
@@ -644,6 +644,7 @@ hr {
 import Vue from "vue";
 import VeeValidate from "vee-validate";
 import gql from "graphql-tag";
+import { statuses } from '../utils';
 import PageHeader from "@/components/PageHeader.vue";
 import Footer from "@/components/Footer.vue";
 import Loader from "@/components/Loader.vue";
@@ -779,10 +780,10 @@ export default Vue.extend({
           `,
           variables: {
             data: {
-              status: "Received",
+              status: statuses.RECEIVED,
               products: { create: this.orderProducts },
               customer: { create: this.customerInfo },
-              shippingAddress: { create: this.shippingInfo },
+              shippingAddress: this.shippingMethod === "delivery" ? { create: this.shippingInfo } : null,
               billingAddress: { create: this.billingInfo },
               payment: { create: this.paymentInfo },
               subtotal: this.subtotal,
--- client/src/views/Confirmation.vue
+++ client/src/views/Confirmation.vue
@@ -4,9 +4,9 @@
     <div v-if="$apollo.loading" class="my-20 text-center"><Loader/></div>
     <div v-else>
       <div class="mt-10 mb-20 container mx-auto">
-          <span class = "text-3xl"> Congrats on your purchase, {{ (order.customer.name.split(" "))[0] }}! 
+          <span class="text-3xl">Congrats on your purchase, {{ (order.customer.name.split(" "))[0] }}!
               Your order was successfully placed.</span> <br><br>
-          <span class = "text-2xl"> Your order is currently <b>{{ order.status }}</b>. </span>
+          <span class="text-2xl">Your order is currently <b>{{ statusDisplayNames[order.status] || order.status }}</b>.</span>
       </div>
     </div>
 
@@ -16,6 +16,7 @@
 <script lang="ts">
 import Vue from "vue";
 import gql from "graphql-tag";
+import { statusDisplayNames } from "../utils";
 import PageHeader from "@/components/PageHeader.vue";
 import Footer from "@/components/Footer.vue";
 import Button from "@/components/Button.vue";
@@ -25,6 +26,7 @@ export default Vue.extend({
   components: { Button, Loader, Footer, PageHeader },
   data() {
     return {
+        statusDisplayNames,
         orderID: this.$route.params.id,
         order: null,
     };
